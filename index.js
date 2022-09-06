7bff9cea194579c3e81900ef42c3b7757b484731
yongjaelim <61070411+yongjaelim@users.noreply.github.com>
2022-09-05 22:23:35 -0700

    1 file modified

--- index.js
+++ index.js
@@ -1,55 +1,47 @@
 // ES6+
 
 class Student {
-  
-
-  constructor (name, enrolled = false) {
-
+  constructor(name, enrolled = false) {
     this.name = name;
     this.enrolled = enrolled;
-
   }
 
   get status() {
-
-    return this.name + " is enrolled:"  + this.enrolled;
+    return this.name + ' is enrolled:' + this.enrolled;
   }
 
-  
-  isEnrolledSlow (delay, callback) {
-
-    setTimeout( () => {
-      if (this.enrolled)
-        callback("Already enrolled", null)
-      else
-        callback(null, this); 
-    } , delay );
-  }  
+  isEnrolledSlow(delay, callback) {
+    setTimeout(() => {
+      if (this.enrolled) callback('Already enrolled', null);
+      else callback(null, this);
+    }, delay);
+  }
 }
 
 const students = Array();
 
-let f = new Student("NameF");
+let f = new Student('NameF');
 
-students.push(f, new Student("NameA"), new Student("NameB"), new Student("NameC"), new Student("NameD"));
+students.push(
+  f,
+  new Student('NameA'),
+  new Student('NameB'),
+  new Student('NameC'),
+  new Student('NameD')
+);
 
 function foo(err, result) {
-
-  if (err)
-    console.log("Error!" + err);
-    else 
-    console.log(result);
+  if (err) console.log('Error!' + err);
+  else console.log(result);
 }
 
-f.isEnrolledSlow(3000, (err, result)=>{
-  if (err)
-    console.log("Error!" + err);
-    else 
-    console.log(result);
+f.isEnrolledSlow(3000, (err, result) => {
+  if (err) console.log('Error!' + err);
+  else console.log(result);
 });
 
 class Course {
-  constructor(name = 'CS0000', enrolled = 0, capacity=25, seats, instructor) {
+  constructor(name = 'CS0000', enrolled = 0, capacity = 25, seats, instructor) {
     this.name = name;
     this.enrolled = enrolled;
     this.capacity = capacity;
@@ -58,85 +50,103 @@ class Course {
   }
 
   toString() {
-    return this.name + " Enrolled: " + this.enrolled + " Capacity: " + this.capacity+ " Seats: " + this.seats + " Instructor: " + this.instructor;
+    return (
+      this.name +
+      ' Enrolled: ' +
+      this.enrolled +
+      ' Capacity: ' +
+      this.capacity +
+      ' Seats: ' +
+      this.seats +
+      ' Instructor: ' +
+      this.instructor
+    );
   }
 
   checkSpaceSlow(delay, callback) {
-    setTimeout (() => {
-      if (this.capacity > this.enrolled)
-        callback(null, this);
-      else
-        callback('Class is full', null);
-
-    } , delay);
+    setTimeout(() => {
+      if (this.capacity > this.enrolled) callback(null, this);
+      else callback('Class is full', null);
+    }, delay);
   }
 }
 
-const course1 = new Course('CS2114', 100, 105, 'Esakia', students);
+const course1 = new Course('CS2114', 100, 105, 'Yongjae', students);
 
 console.log(course1.toString());
 
-
 obj = {
-  username : "yongjae",
-  password : "123"
-}
+  username: 'yongjae',
+  password: '123',
+};
 
 console.log(obj.password);
 
 const course2 = {
-
-  name:'CS2104',
+  name: 'CS2104',
   enrolled: 51,
   capacity: 52,
-  seats:55,
-  instructor:'Esakia',
-  toString: function() {
-    return this.name + " Enrolled: " + this.enrolled + " Capacity: " + this.capacity+ " Seats: " + this.seats + " Instructor: " + this.instructor;
+  seats: 55,
+  instructor: 'Esakia',
+  toString: function () {
+    return (
+      this.name +
+      ' Enrolled: ' +
+      this.enrolled +
+      ' Capacity: ' +
+      this.capacity +
+      ' Seats: ' +
+      this.seats +
+      ' Instructor: ' +
+      this.instructor
+    );
   },
-  checkSpaceSlow: function(delay, callback) {
-    setTimeout (() => {
-      if (this.capacity > this.enrolled)
-        callback(null, this);
-      else
-        callback('Class is full', null);
+  checkSpaceSlow: function (delay, callback) {
+    setTimeout(() => {
+      if (this.capacity > this.enrolled) callback(null, this);
+      else callback('Class is full', null);
+    }, delay);
+  },
+};
 
-    } , delay);
-  }
- 
-}
-
-const course3 = Object.assign( Object.create(Course.prototype), {
-  name:'CS2304',
+const course3 = Object.assign(Object.create(Course.prototype), {
+  name: 'CS2304',
   enrolled: 49,
   capacity: 57,
-  seats:60,
-  instructor:'Esakia',
-  toString: function() {
-    return this.name + " Enrolled: " + this.enrolled + " Capacity: " + this.capacity+ " Seats: " + this.seats + " Instructor: " + this.instructor;
+  seats: 60,
+  instructor: 'Esakia',
+  toString: function () {
+    return (
+      this.name +
+      ' Enrolled: ' +
+      this.enrolled +
+      ' Capacity: ' +
+      this.capacity +
+      ' Seats: ' +
+      this.seats +
+      ' Instructor: ' +
+      this.instructor
+    );
   },
-  checkSpaceSlow: function(delay, callback) {
-    setTimeout (() => {
-      if (this.capacity > this.enrolled)
-        callback(null, this);
-      else
-        callback('Class is full', null);
+  checkSpaceSlow: function (delay, callback) {
+    setTimeout(() => {
+      if (this.capacity > this.enrolled) callback(null, this);
+      else callback('Class is full', null);
+    }, delay);
+  },
+});
 
-    } , delay);}
-  });
+let courses = new Array();
 
-  let courses = new Array();
-
-  function addToCourseArray(courses, course) {
-    if(course instanceof Course) {
-      courses.push(course);
-      console.log("Course was added!");
-    }
-    else {
-      console.log("wrong type!");
-    }
+function addToCourseArray(courses, course) {
+  if (course instanceof Course) {
+    courses.push(course);
+    console.log('Course was added!');
+  } else {
+    console.log('wrong type!');
   }
+}
 
-  addToCourseArray(courses, course1);
-  addToCourseArray(courses, course2);
-  addToCourseArray(courses, course3);
+addToCourseArray(courses, course1);
+addToCourseArray(courses, course2);
+addToCourseArray(courses, course3);
